77b053267063910fcce51ef005dad203916edf82
Hiroshi Kikuchi <kikuchy.h@gmail.com>
2012-12-08 01:12:10 +0900

    Initial Commit

---
+++ DataBindingBridge/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataBindingBridge
+//
+//  Created by 菊池 紘 on 12/12/08.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
